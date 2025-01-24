de055b3343f51b20eb04b3de1f22f4c9f7ffabd5
Michał Święciło <michal_00-00@o2.pl>
2025-01-24 14:11:57 +0100

    Room & Message models created

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    """Model representing a chat room associated with owner."""
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_rooms")
+    guests = models.ManyToManyField(User, blank=True, related_name="guest_rooms")
+    favorited_by = models.ManyToManyField(User, blank=True, related_name="favorite_rooms")
+    is_owner_only_editable = models.BooleanField(default=True)
+    is_publicly_visible = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} (owner: {self.owner.username})"
+
+    class Meta:
+        """Meta options for the Room model."""
+        ordering = ["-created_at"]
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+
+
+class Message(models.Model):
+    """Model representing single, user's message in the chat."""
+    room = models.ForeignKey("Room", on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
+    content = models.TextField(blank=True, null=True)
+    image_blob = models.BinaryField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.content and not self.image_blob:
+            raise ValueError("Message must contain text or image.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        if self.image_blob:
+            return f"{self.sender.username} in {self.room.name} at {self.created_at}: [image]"
+        else:
+            return f"{self.sender.username} in {self.room.name} at {self.created_at}: {self.content[:30]}"
+
+    class Meta:
+        """Meta options for Room model."""
+        ordering = ["-created_at"]
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
+    
