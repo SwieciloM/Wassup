ce28bddee5073e169ec4e899dc930459a9539a8a
Michał Święciło <michal_00-00@o2.pl>
2025-03-05 23:24:57 +0100

    Room joining function enabled

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import RoomListView, RoomDetailView, RoomCreateView, RoomToogleFavouriteView, RoomLeaveView, RoomUpdateView, RoomDeleteView
+from .views import RoomListView, RoomDetailView, RoomCreateView, RoomToogleFavouriteView, RoomLeaveView, RoomUpdateView, RoomDeleteView, RoomJoinView
 
 urlpatterns = [
     path('', RoomListView.as_view(), name='home'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('room-delete/<int:pk>/', RoomDeleteView.as_view(), name='room-delete'),
     path('room-toggle-favourite/<int:pk>/', RoomToogleFavouriteView.as_view(), name='room-toggle-favourite'),
     path('room-leave/<int:pk>/', RoomLeaveView.as_view(), name='room-leave'),
+    path('room-join/<int:pk>/', RoomJoinView.as_view(), name='room-join'),
 ]
