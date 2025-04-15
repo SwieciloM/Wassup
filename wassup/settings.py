f82a9bd660865b6c156a85c3ae05901b6e633475
Michał Święciło <michal_00-00@o2.pl>
2025-04-15 19:54:42 +0200

    Binding the MEDIA_ROOT variable with the environment type

--- wassup/settings.py
+++ wassup/settings.py
@@ -150,8 +150,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files
+MEDIA_ROOT = env('MEDIA_ROOT', default=BASE_DIR/'media')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
